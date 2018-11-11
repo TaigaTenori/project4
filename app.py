f3f9be4732d878972484b90aa200ae8850749280
Piotr Nowakowski <nowakowski.peter@gmail.com>
2018-11-11 15:56:42 +0000

    Materialize icons now work properly in mobile

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run(host = os.getenv('IP'),
-            port = os.getenv('PORT'),
+    app.run(host = os.getenv('IP', '0.0.0.0'),
+            port = os.getenv('PORT', '8080'),
             debug = True
             )
