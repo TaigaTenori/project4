f630d5faefbbf1cac01275cdf150f5ef5e510271
Piotr Nowakowski <nowakowski.peter@gmail.com>
2018-11-11 20:37:31 +0000

    Added mongo connectivity and recipies fetching

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 from flask import Flask, render_template
 import os
-
+from flask_pymongo import PyMongo
 
 app = Flask(__name__)
 
+app.config['MONGO_DBNAME'] = "cookbook"
+app.config['MONGO_URI'] = "mongodb://admin:verysecret12@ds241012.mlab.com:41012/cookbook"
 
+mongo = PyMongo(app)
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', recipies = mongo.db.recipies.find())
 
 
 if __name__ == '__main__':
