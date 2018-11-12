4cf96b00a6d13fdd86cec73f1bb30c19879f3d00
Piotr Nowakowski <nowakowski.peter@gmail.com>
2018-11-12 02:34:07 +0000

    Added new page for adding recipies Imlemented reading categories from database and filling them into a select element of the form

--- app.py
+++ app.py
@@ -12,6 +12,9 @@ mongo = PyMongo(app)
 def index():
     return render_template('index.html', recipies = mongo.db.recipies.find())
 
+@app.route('/add_recipe')
+def add_recipe():
+    return render_template('add_recipe.html', categories = mongo.db.categories.find())
 
 if __name__ == '__main__':
     app.run(host = os.getenv('IP', '0.0.0.0'),
