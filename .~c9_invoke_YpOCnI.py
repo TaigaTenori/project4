81676ed0f2f2906177c1c7f9e5b1c6a8ebd4e6b9
Piotr Nowakowski <nowakowski.peter@gmail.com>
2018-11-13 00:44:48 +0000

    Added some validity checks in js, also the fields for ingredient and quantity will now be automaticaly emptied when added

---
+++ .~c9_invoke_YpOCnI.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+import os
+from flask_pymongo import PyMongo
+
+app = Flask(__name__)
+
+app.config['MONGO_DBNAME'] = "cookbook"
+app.config['MONGO_URI'] = "mongodb://admin:verysecret12@ds241012.mlab.com:41012/cookbook"
+
+mongo = PyMongo(app)
+@app.route('/')
+def index():
+    return render_template('index.html', recipies = mongo.db.recipies.find())
+
+@app.route('/add_recipe')
+def add_recipe():
+    return render_template('add_recipe.html', categories = mongo.db.categories.find())
+
+if __name__ == '__main__':
+    app.run(host = os.getenv('IP', '0.0.0.0'),
+            port = os.getenv('PORT', '8080'),
+            debug = True
+            )
--- static/js/script.js
+++ static/js/script.js
@@ -2,14 +2,23 @@
 
 $(document).ready(function(){
     $('#add_recipe').click(function (){
-        var ing = $('#ingredient').val();
-        var quantity = $('#quantity').val();
+        var x =  $('#ingredient');
+        var y =  $('#quantity');
         
+        var ing = x.val();
+        var quantity = y.val();
+        
+        if (ing == '' || quantity == '') {
+            window.alert('Both Ingredient and Quantity fields must not be empty');
+            return;
+        }
         var list = $('#ingredients');
         var txt = list.val();
         
-        list.val(txt + '\n' + ing + ':' + quantity);
+        list.val(txt + '\n' + ing + ': ' + quantity);
         
+        x.val('');
+        y.val('');
         M.textareaAutoResize($('#ingredients'));
         
     });
