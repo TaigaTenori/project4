cee3f57cac4bbf53ff81fc0f57921e11f7afd74d
Piotr Nowakowski <nowakowski.peter@gmail.com>
2018-11-21 04:49:22 +0000

    Removing added ingredients|

--- static/js/script.js
+++ static/js/script.js
@@ -1,19 +1,28 @@
 /* global $ */
+$(document).delegate('[id^=remove_ingredient]', 'click', function()
+{
+        console.log('click');
+       $(this).closest('.added_div').remove(); 
+});
 
 $(document).ready(function(){
     var click = 0;
+
     $('#add_ingredient').click(function (){
         
-        var a = $(`<div id="ingredients">
+        var a = $(`<div class="added_div" id="row">
                <div class="ing1 input-field col s6">
                   <i class="material-icons prefix">flag</i>
                   <input name="ingredient${click}" id="ing${click}" type="text">
                   <label for="ingredient${click}">Ingredient</label>
                 </div>
-               <div class="quant input-field col s6">
+               <div class="quant input-field col s3">
                   <i class="material-icons prefix">flag</i>
                   <input name="quantity${click}" id="quant${click}" type="text">
                   <label for="quantity${click}">Quantity</label>
+                </div>
+               <div class="input-field col s3">
+                    <a id="remove_ingredient${click}" class="btn-floating btn-small waves-effect waves-light red"><i class="material-icons">indeterminate_check_box</i></a>
                 </div>
             </div>`);
 
